62db69f8ff66d8db1c0130d7d9cd1e98cec2d360
heffnercj <heffnercj@cdec9f00-043a-908e-c854-9f98b316d3d5>
2012-04-22 02:39:46 +0000

    Added ChipID method to ParallelFlash class.

--- src/ng/python/examples/pflashdump.py
+++ src/ng/python/examples/pflashdump.py
@@ -8,29 +8,54 @@ class ParallelFlash(Parallel):
 	DEFAULT_TSCE = 10000000
 
 	def ReadChip(self, address, count):
+		"""
+		Reads count bytes from the target chip starting at address.
+		"""
                 return self.Read(address, count)
 
         def WriteChip(self, address, data):
+		"""
+		Writes data to the target chip starting at address.
+		"""
                 self.config.SetCommand("WRITE")
                 return self.Write(address, data)
 
+	def ChipID(self):
+		"""
+		This is a generic method to obtain the vendor and product ID of parallel flash chips.
+		It may not work for some chips; for others, you may need to connect an external high voltage power source to certian pins. 
+		Consult your chip's datasheet.
+
+		Returns a tuple of (vendor id, product id).
+		"""
+		self.config.SetCommand("ID")
+		vendor = ord(self.Read(0, 1)[0])
+		product = ord(self.Read(1, 1)[0])
+		return (vendor, product)
+
         def EraseChip(self):
+		"""
+		Perform a full erase of the target chip.
+		"""
                 self.config.SetCommand("ERASE")
 
+		#TODO: The maximum possible TSCE delay is only a few seconds.
+		#      Need a wrapper in the AVR code to handle londer delays for chip erases.
 		try:
 			self.config.CONFIG["CMDELAY"] = self.config.CONFIG["TSCE"]
 		except:
 			self.config.CONFIG["CMDELAY"] = [self.DEFAULT_TSCE]
 
-		# TODO: Why does ExecuteCommands need to be invoked twice for the erase to work?
+		# TODO: Why does ExecuteCommands() need to be invoked twice for the erase to work?
 		self.ExecuteCommands()
 		self.ExecuteCommands()
-
 		return True
 
 if __name__ == "__main__":
+
 	pflash = ParallelFlash(config="config/39SF020.conf")
-	data = pflash.ReadChip(0, 1024)
+	vendor, product = pflash.ChipID()
 	pflash.Close()
 
-	open("flash.bin", "w").write(data)
+	print "Vendor ID:  0x%X" % vendor
+	print "Product ID: 0x%X" % product
