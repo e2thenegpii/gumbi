dc97ef5e356d41972e07b5a6173ac4dc672eb016
heffnercj <heffnercj@cdec9f00-043a-908e-c854-9f98b316d3d5>
2012-05-01 13:18:27 +0000

    Initial check in of stream files.

---
+++ src/avr/stream.c
@@ -0,0 +1,37 @@
+#include "stream.h"
+
+/* Read from all pins and stream data back to the host n number of times */
+void stream(void)
+{
+	uint32_t i = 0, count = 0;
+	uint8_t j = 0, gpioa = 0, gpiob = 0;
+
+	mcp23s17_enable();
+
+	while(TRUE)
+	{
+		read_data((uint8_t *) &count, 4);
+
+		if(count == 0)
+		{
+			ack();
+			break;
+		}
+
+		for(i=0; i<count; i++)
+		{
+			for(j=0; j<gconfig.num_io_devices; j++)
+			{
+				gpioa = read_register(j, GPIOA);
+				gpiob = read_register(j, GPIOB);
+
+				buffered_write((uint8_t *) &gpioa, 1);
+				buffered_write((uint8_t *) &gpiob, 1);
+			}
+		}
+
+		flush_buffer();
+	}
+
+	mcp23s17_disable();
+}
---
+++ src/avr/stream.h
@@ -0,0 +1,9 @@
+#ifndef __STREAM_H__
+#define __STREAM_H__
+
+#include "common.h"
+#include "mcp23s17.h"
+
+void stream(void);
+
+#endif
