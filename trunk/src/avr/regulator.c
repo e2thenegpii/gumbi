96f36f2efffc95c30f04f513542d6937308bf3fb
heffnercj <heffnercj@cdec9f00-043a-908e-c854-9f98b316d3d5>
2012-05-09 15:58:22 +0000

    Initial check-in of regulator control code.

---
+++ trunk/src/avr/regulator.c
@@ -0,0 +1,43 @@
+#include "regulator.h"
+
+/* Initialize regulators; defaults to 4.7v */
+void regulator_init(void)
+{
+	REGULATOR_DDR |= (1 << V47_PIN) | (1 << V30_PIN) | (1 << V18_PIN);
+
+	set_regulator(V47);
+}
+
+/* Handler for the VOLTAGE command */
+void voltage(void)
+{
+	uint8_t v = 0;
+
+	read_data((uint8_t *) &v, sizeof(v));
+	set_regulator(v);
+	ack();
+}
+
+/* Enable the appropriate regulator */
+void set_regulator(uint8_t voltage)
+{
+	/* Disable all voltage regulators */
+	REGULATOR_PORT &= ~(1 << V47_PIN) & ~(1 << V30_PIN) & ~(V18_PIN);
+
+	/* Enable the selected regulator */
+	switch(voltage)
+	{
+		case V47:
+			REGULATOR_PORT |= (1 << V47_PIN);
+			break;
+		case V30:
+			REGULATOR_PORT |= (1 << V30_PIN);
+			break;
+		case V18:
+			REGULATOR_PORT |= (1 << V18_PIN);
+			break;
+		case V0:
+		default:
+			break;
+	}
+}
