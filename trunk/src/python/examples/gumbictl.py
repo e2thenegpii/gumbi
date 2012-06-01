78772ce03d0e53373426ece6f66f6aaa59989103
heffnercj <heffnercj@cdec9f00-043a-908e-c854-9f98b316d3d5>
2012-06-01 15:02:27 +0000

    Minor changes to flashbin.py.

--- trunk/src/python/examples/gumbictl.py
+++ trunk/src/python/examples/gumbictl.py
@@ -2,7 +2,7 @@
 
 import sys
 from time import sleep
-from gumbi import Gumbi, GPIO, Info, ScanBus
+from gumbi import *
 
 
 def blinki():
@@ -50,11 +50,24 @@ def info():
 		print line
 	i.Close()
 
+def ping():
+	p = Ping()
+	print "Ping successful:", p.Ping()
+	p.Close
+
+def speed(c):
+	s = SpeedTest(c)
+	print "Performing speed test with", c, "bytes..."
+	t = s.Go()
+	print "Transferred", c, "bytes in", t, "seconds."
+	print "Data transfer valid:", s.Validate()
+	s.Close()
+
 
 
 if __name__ == '__main__':
 	def usage():
-		print "Usage: %s [--info | --led | --scan | --voltage <0|2|3|5>]" % sys.argv[0]
+		print "Usage: %s [--info | --led | --scan | --ping | --speed-test <# of bytes> | --voltage <0|2|3|5>]" % sys.argv[0]
 		sys.exit(1)
 
 	def main():
@@ -63,9 +76,13 @@ if __name__ == '__main__':
 				info()
 			elif sys.argv[1] == '--led':
 				blinki()
+			elif sys.argv[1] == '--ping':
+				ping()
 			elif sys.argv[1] == '--scan':
 				scan()
 				info()
+			elif sys.argv[1] == '--speed-test':
+				speed(int(sys.argv[2]))
 			elif sys.argv[1] == '--voltage':
 				voltage(int(sys.argv[2]))
 				info()
