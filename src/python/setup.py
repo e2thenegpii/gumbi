1c670cd46ef900d4fca8d4b9aecbea5c4de45dfd
heffnercj <heffnercj@cdec9f00-043a-908e-c854-9f98b316d3d5>
2012-06-04 18:32:24 +0000

    Added install scripts.

---
+++ src/python/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(	name='Gumbi',
+	version='1.1',
+	description='Gumbi API and utilities',
+	author='Craig Heffner',
+	author_email='cheffner@tacnetsol.com',
+	url='http://gumbi.googlecode.com',
+	packages=['gumbi'],
+)
